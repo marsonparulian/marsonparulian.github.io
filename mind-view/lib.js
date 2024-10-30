84d516740bc000f6580f71c5af06c4f1906c207c
marson parulian <marson.parulian@gmail.com>
2024-10-30 11:14:20 +1100

    Define similar colors

--- mind-view/lib.js
+++ mind-view/lib.js
@@ -1,3 +1,30 @@
 /**
  * This file contains the library of functions for the MindView project.
  */
+// Define pairs of similar colors
+const similarColors = [
+    ["red", "pink"],
+    ["red", "orange"],
+    ["blue", "green"]
+    ["blue", "lightblue"],
+    ["blue", "teal"],
+    ["orange", "brown"],
+    ["orange", "yellow"],
+    ["green", "lime"],
+    ["green", "teal"],
+    ["gray", "darkgray"],
+    ["black", "dimgray"],
+    ["white", "snow"]
+];
+
+// More advance similar colors
+const similarColorsAdvanced = [
+    ["teal", "turquoise"],
+    ["yellow", "gold"],
+    ["orange", "coral"],
+    ["purple", "violet"],
+    ["brown", "sienna"],
+];
+
+
+
